cd0be9d8f804184020f43271324c532f3c75b9bc
Candui <serapup2@mail.ru>
2022-08-11 16:30:35 +0300

    changed placement of carousel

--- src/routes/Commerce/commerce.component.jsx
+++ src/routes/Commerce/commerce.component.jsx
@@ -6,6 +6,7 @@ const Commerce = () => {
        <div>
        <Navigation/>
 
+       
 
        Страница о комерции
        
--- src/routes/Home/home.styles.jsx
+++ src/routes/Home/home.styles.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const CarouselContainer = styled.div`
   height: 100%;
   width: 100%;
-  display: flex;
   margin-left: auto;
   margin-right: auto;
   margin-bottom: 25px;
