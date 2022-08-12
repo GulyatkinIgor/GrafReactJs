29978fb013f7efb37a625a5eed97c96f00d6d320
Gulyatkin Igor <elton42ig@gmail.com>
2022-08-12 23:49:37 +0300

    PIZDA

--- src/routes/Sales/salesCard-list/card-list.component.jsx
+++ src/routes/Sales/salesCard-list/card-list.component.jsx
@@ -2,14 +2,14 @@ import SaleCard from '../salesCard/card.component';
 import './card-list.styles.css';
 
 const SaleCardList = ({ dataAparts }) => {
-  const {Category,Id} = dataAparts;
 
   return(
     <div className='card-list'>
         {dataAparts.map((Category,Id)=>{
-          console.log(dataAparts.Category)
+          console.log(dataAparts[1].Category._text)
           return(
-            <h2>Category: {dataAparts.Category._text}</h2>
+            <h2>Category: {Category.Category._text}</h2>
+            
            
           )
 
