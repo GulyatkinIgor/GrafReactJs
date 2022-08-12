eb4148fae54f91b9a029b87b00719e95e171ff2f
Candui <serapup2@mail.ru>
2022-08-12 21:54:31 +0300

    ads

--- src/routes/Sales/salesCard-list/card-list.component.jsx
+++ src/routes/Sales/salesCard-list/card-list.component.jsx
@@ -1,13 +1,24 @@
 import SaleCard from '../salesCard/card.component';
 import './card-list.styles.css';
 
-const SaleCardList = ({ dataAparts }) => (
-  <div className='card-list'>
-    {dataAparts.map((dataApart) => {
-      console.log("SaleCardData", dataAparts)
-      return <SaleCard key={dataApart.id._text} dataApart={dataApart} />;
-    })}
-  </div>
-);
+const SaleCardList = ({ dataAparts }) => {
+  const {Category,Id} = dataAparts;
 
+  return(
+    <div className='card-list'>
+        {dataAparts.map((Category,Id)=>{
+          console.log(dataAparts.Category)
+          return(
+            <h2>Category: {dataAparts.Category._text}</h2>
+           
+          )
+
+        })}
+     
+    </div>
+    )
+};
+  
+
+   
 export default SaleCardList;
