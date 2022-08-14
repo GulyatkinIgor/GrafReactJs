e9b3712253d0e230219421cb795576085ebeca66
Gulyatkin Igor <elton42ig@gmail.com>
2022-08-15 00:22:51 +0300

    update

--- src/routes/Sales/salesCard-list/card-list.component.jsx
+++ src/routes/Sales/salesCard-list/card-list.component.jsx
@@ -5,12 +5,19 @@ const SaleCardList = ({ dataAparts }) => {
 
   return(
     <div className='card-list'>
-        {dataAparts.map((Category,Id)=>{
-          console.log(dataAparts[1].Category._text)
-          return(
-            <h2>Category: {Category.Category._text}</h2>
-            
-           
+        {dataAparts.map((data,Id)=>{
+          return(            
+            <div>
+        <a class="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"/>
+        </a>
+        <div class="mt-4">
+          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{data.Locality._text}</h3>
+          <h2 class="text-gray-900 title-font text-lg font-medium">{data.Title._text}</h2>
+          <p class="mt-1">{data.Price._text}</p>
+        </div>
+      </div>
+  
           )
 
         })}
@@ -22,3 +29,4 @@ const SaleCardList = ({ dataAparts }) => {
 
    
 export default SaleCardList;
+
--- src/routes/Sales/salesCard/card.component.jsx
+++ src/routes/Sales/salesCard/card.component.jsx
@@ -21,3 +21,13 @@ const SaleCard = ({ dataAparts }) => {
 
 export default SaleCard;
 
+
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    
+    <div class="flex flex-wrap -m-4">
+    </div>
+  </div>
+</section>
+
