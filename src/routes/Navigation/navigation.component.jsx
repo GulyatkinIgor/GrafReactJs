7ea99deb3f0ae913d4050d7b15f05e5ebf807432
Candui <serapup2@mail.ru>
2022-08-14 16:30:22 +0300

    styles chenged added vk svg

--- src/routes/Navigation/navigation.component.jsx
+++ src/routes/Navigation/navigation.component.jsx
@@ -18,7 +18,7 @@ const Navigation = () => {
     <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
         <div class="relative flex flex-col md:flex-row">
             <a href="/" class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
-                <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">tails<span class="text-indigo-600">.</span></span>
+                <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">Граф</span>
             </a>
             <nav class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
                 <a href="uslugi" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">Услуги</a>
@@ -32,7 +32,7 @@ const Navigation = () => {
         </div>
 
         <div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
-            <a href="#" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
+            <a href="#" class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-black border border-transparent rounded-md shadow-sm hover:bg-gray-500 ">
             г.Казань ул.Ф.Амирхана 12А 89520379479
             </a>
         </div>
