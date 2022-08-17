3f6e54f71273461a3f35ee08a11d68ebd5de02e6
Gulyatkin Igor <elton42ig@gmail.com>
2022-08-18 02:19:42 +0300

    3213

--- src/routes/Sales/component/modal.component.jsx
+++ src/routes/Sales/component/modal.component.jsx
@@ -1,12 +1,30 @@
 import React, { Component, useState } from "react";
 import Loading from "../../../Loading";
+import ImageGallery from 'react-image-gallery';
+@import "~react-image-gallery/styles/css/image-gallery.css";
 import "./modal.css"
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
 const ModalItem = (props) => {
 console.log("ITEM DATA", props.data)
   return props.isLoad ? <Loading/> : (
     <div className={props.active ? "modal active" : "modal"} onClick={()=> props.setActive (false)}>
       <div className={props.active ? "modalContent active" : "modalContent"} onClick={e => e.stopPropagation()}>
+        <ImageGallery items={images} />
         <h1>{props.data.Description._text}</h1>
       </div>
     </div>
