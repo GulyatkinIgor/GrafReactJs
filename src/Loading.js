66cf0a4fc68382abb1219636b5b03f4f9585d927
Gulyatkin Igor <elton42ig@gmail.com>
2022-08-18 20:08:14 +0300

    wqe

--- src/Loading.js
+++ src/Loading.js
@@ -2,12 +2,6 @@ import Footer from "./routes/Footer/footer.component";
 import Navigation from "./routes/Navigation/navigation.component";
 
 const Loading = () => {
-  return (
-    <div>
-      <Navigation />
-      Loading...
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 };
 export default Loading;
